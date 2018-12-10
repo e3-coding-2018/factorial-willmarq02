105554cca6fec183de24f31b9a2eb765b1b0ad8b
Stephen Cerruti <scerruti@e3civichigh.com>
2018-12-10 12:20:28 -0800

    Initial version

---
+++ src/com/e3civichigh/coding/Factorial.java
@@ -0,0 +1,32 @@
+package com.e3civichigh.coding;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        final int NUM_FACTS = 100;
+
+        // TODO 1: Please create a for loop that counts from 0 up to NUM_FACTS
+        for () {
+            System.out.println(i + "! is " + factorial(i));
+        }
+    }
+
+    /**
+     * factorial computes n!
+     *
+     * @param n
+     * @return n factorial
+     */
+    public static int factorial(int n)
+    {
+        int result = 1;
+
+        // TODO 2: Please create a foor loop that counts from 2 up to and including n
+        for() {
+            result *= i;
+        }
+
+        return result;
+    }
+}
